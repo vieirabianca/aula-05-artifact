f9b08a2d6345da35f56f5ca7c929ce354a8c0867
vieirabianca <bianca.vieira1996@outlook.com>
2024-09-10 15:29:54 -0300

    Create hello-server.go

---
+++ hello-server.go
@@ -0,0 +1,18 @@
+// Credits:
+// https://yourbasic.org/golang/http-server-example/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":13000", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
